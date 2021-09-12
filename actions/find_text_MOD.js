71445cc5da71692b9e760398908406770d2f6559
MineEjo <evgenyshaimov@gmail.com>
2021-09-12 15:50:11 +0300

    Update find_text_MOD.js

--- actions/find_text_MOD.js
+++ actions/find_text_MOD.js
@@ -10,7 +10,7 @@ module.exports = {
         if (parseInt(data.storage, 10) !== varType) return;
         return [data.varName, 'Fined Text'];
     },
-    fields: ['text', 'toFind', 'option', 'storage', 'varName', 'title'],
+    fields: ['text', 'toFind', 'option', 'additionally', 'storage', 'varName', 'title'],
 
     html(isEvent, data) {
         return `
@@ -18,31 +18,28 @@ module.exports = {
 <div>
     <details>
         <summary style="cursor: pointer">Find Text Mod Description</summary>
-        [Version 1.1] [<a href="#" onclick="DBM.openLink('https://github.com/MinEjo-DBM')">GitHub</a>]<br>
+        [Version 1.2] [<a href="#" onclick="DBM.openLink('https://github.com/MinEjo-DBM')">GitHub</a>]<br>
         Text search, text positioning, and all together. 
-        <div style="padding: 0 15px 7px 15px; background: rgba(0,0,0,0.13); border-radius: 3px">
-            <br><p>
-                The text position of type <span style="color: #3374d7; padding-left: 3px; padding-right: 3px;">1,4</span> is an array, and <span style="color: #3374d7; padding-left: 3px; padding-right: 3px;">1</span> is the <span style="color: #3374d7; padding-left: 3px; padding-right: 3px;">starting point</span> and <span style="color: #3374d7; padding-left: 3px; padding-right: 3px;">4</span> is the<span style="color: #3374d7; padding-left: 3px; padding-right: 3px;"> end point</span>.
-                <p>Word: Hello [str]<br>
-                Word position: 0,5 [array]<br>
-                Word + position: Hello,0,5 [array]<br>
-                Range: Hello [str]<br>
-                Position range: Hello [str]</p></p>
-        </div>
     </details>
 </div>
-    <div style="float: left; width: 65%; padding-top: 8px;">
+    <div style="float: left; width: 100%; padding-top: 8px;">
       Source Text:
       <input id="text" class="round" placeholder="Insert text here..." type="text"/>
     </div>
-    <div style="float: right; width: 29%; padding-top: 8px;">
+    <div style="float: left; width: 45%; padding-top: 8px;">
       Option:<br>
       <select onchange="glob.changeOption(this.value);" id="option" class="round">
         <option value="0" selected>Word</option>
-        <option value="1">Word position</option>
+        <option value="1">Range</option>
+        <option value="2">Positions</option>
+    </select>
+    </div>
+    <div style="float: right; width: 50%; padding-top: 8px;">
+      Additional Option:<br>
+      <select id="additionally" class="round">
+        <option value="0" selected>Nothing</option>
+        <option value="1">Position</option>
         <option value="2">Word + position</option>
-        <option value="3">Range</option>
-        <option value="4">Position range</option>
     </select>
     </div>
     <div style="float: left; width: 99%; padding-top: 8px;">
@@ -66,7 +63,7 @@ module.exports = {
         const {glob, document} = this;
 
         glob.changeOption = function (value) {
-            const text = ['Insert text here...', 'Insert text here...', 'Insert text here...', 'Insert the first, second word here... [Example: hello,bye]', 'Insert here the first and second position... [Example: 0,5]'];
+            const text = ['Insert text here...', 'Insert the first, second word here... [Example: hello,bye]', 'Insert here the first and second position... [Example: 0,5]'];
             document.getElementById('title').value = text[value];
         }
 
@@ -79,6 +76,7 @@ module.exports = {
         const toFind = this.evalMessage(data.toFind, cache);
         this.evalMessage(data.title, cache);
         const option = parseInt(data.option, 10);
+        const additionally = parseInt(data.additionally, 10);
 
         if (!toFind) return console.log('Find Text MOD: Text to find is missing!');
         if (!text) return console.log('Find Text MOD: Source text is missing!');
@@ -86,25 +84,27 @@ module.exports = {
         let result;
         switch (option) {
             case 0:
-                result = text.slice(text.indexOf(toFind), toFind.length + text.indexOf(toFind));
+                if (additionally === 0) result = text.slice(text.indexOf(toFind), toFind.length + text.indexOf(toFind));
+                if (additionally === 1) result = [text.indexOf(toFind), toFind.length + text.indexOf(toFind)];
+                if (additionally === 2) result = [text.slice(text.indexOf(toFind), toFind.length + text.indexOf(toFind)), text.indexOf(toFind), toFind.length + text.indexOf(toFind)]
                 break;
             case 1:
-                result = [text.indexOf(toFind), toFind.length + text.indexOf(toFind)];
+                let arrayWords = toFind.split(",");
+                if (additionally === 0) result = text.slice(text.indexOf(arrayWords[0]), text.indexOf(arrayWords[1]));
+                if (additionally === 1) result = [text.indexOf(arrayWords[0]), text.indexOf(arrayWords[1])];
+                if (additionally === 2) result = [text.slice(text.indexOf(arrayWords[0]), text.indexOf(arrayWords[1])), text.indexOf(arrayWords[0]), text.indexOf(arrayWords[1])];
                 break;
             case 2:
-                result = [text.slice(text.indexOf(toFind), toFind.length + text.indexOf(toFind)), text.indexOf(toFind), toFind.length + text.indexOf(toFind)];
-                break;
-            case 3:
-                let arrayWords = toFind.split(",");
-                result = text.slice(text.indexOf(arrayWords[0]), text.indexOf(arrayWords[1]));
-                break;
-            case 4:
                 let arrayNumbers = toFind.split(",");
-                result = text.slice(arrayNumbers[0], arrayNumbers[1]);
+                if (additionally === 0) result = text.slice(arrayNumbers[0], arrayNumbers[1]);
+                if (additionally === 1) result = [arrayNumbers[0], arrayNumbers[1]];
+                if (additionally === 2) result = [text.slice(arrayNumbers[0], arrayNumbers[1]), arrayNumbers[0], arrayNumbers[1]];
                 break;
         }
-        if (!text.includes(toFind) && (option === 0 || option === 1 || option === 2)) result = 'undefined';
-        if ((option === 3 || option === 4) && result.length === 0) result = 'undefined';
+        if (result !== undefined) {
+            if ((!text.includes(toFind) && option === 0) || result.length === 0) { result = 'undefined'; }
+            if ((option === 1 || option === 2) && result.length === 0) { result = 'undefined'; }
+        }
 
         const storage = parseInt(data.storage, 10);
         const varName = this.evalMessage(data.varName, cache);
